3bd620991cc0838bc17a7f39cf79f3b601ba9340
strozilla <74827509+strozilla@users.noreply.github.com>
2021-10-02 12:14:11 -0500

    update Layout and Index

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,28 +6,26 @@ import Layout from '../components/Layout';
 
 import Scroll from '../components/Scroll';
 
- 
-
 import config from '../../config';
 const IndexPage = () => (
-  <Layout>
-    <section id="banner">
-          <div id="zilla"></div>
-      <div className="inner">
-        <h2>{config.heading}</h2>
-        <p>{config.subHeading}</p>
-        <ul className="actions special">
-          <li>
-            <Scroll type="id" element="projects">
-              <Link href="./Projects/" className="button primary">
-                Explore
-              </Link>
-            </Scroll>
-          </li>
-        </ul>
-      </div>
-    </section>
-  </Layout>
+	<Layout>
+		<section id="banner">
+			<div id="zilla"></div>
+			<div className="inner">
+				<h2>{config.heading}</h2>
+				<p>{config.subHeading}</p>
+				<ul className="actions special">
+					<li>
+						<Scroll type="id" element="projects">
+							<Link to="/Projects/" className="button primary">
+								Explore
+							</Link>
+						</Scroll>
+					</li>
+				</ul>
+			</div>
+		</section>
+	</Layout>
 );
 
 export default IndexPage;
