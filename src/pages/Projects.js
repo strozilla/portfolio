c98adf462b769d4c0c6407eb3e7b15b0e81a859c
strozilla <74827509+strozilla@users.noreply.github.com>
2021-10-08 17:42:18 -0500

    update projects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Layout from '../components/Layout';
 
-import pic1 from '../assets/img/vidzproj.png';
+import pic1 from '../assets/img/42comingsoon.png';
 import pic2 from '../assets/img/landingpage.png';
 import pic3 from '../assets/img/GSLogo.png';
 
@@ -13,19 +13,26 @@ const IndexPage = () => (
 			</header>
 			<section className="spotlight">
 				<div className="image">
-					<img src={pic1} alt="video-finder" />
+					<img src={pic1} alt="42BAT" />
 				</div>
 				<div className="content">
-					<h2>Definitely NOT Youtube</h2>
+					<h2>42 Bar and Table</h2>
 					<p>
-						<a
-							href="https://strozilla.github.io/searchvidz/"
-							target="_blank"
-							rel="noreferrer"
-						>
-							Website that utilizes Youtube API.
-						</a>
+						'(As of October 2021)' Currently working on redesign and Integration
+						of 42 Bar and Table website for The Clinton Foundation.
 					</p>
+				</div>
+			</section>
+
+			<section className="spotlight">
+				<div className="image">
+					<img src={pic3} alt="Gold+Sage Logo" />
+				</div>
+				<div className="content">
+					<h2>
+						<a href="">Gold + Sage Jewelry Website</a>
+					</h2>
+					<p>Gold + Sage eCommerce website coming soon!</p>
 				</div>
 			</section>
 			<section className="spotlight">
@@ -40,21 +47,10 @@ const IndexPage = () => (
 							target="_blank"
 							rel="noreferrer"
 						>
-							Simple. Sleek. Modern. Get More Downloads By Wowing
-							Customers With A Captivating Landing Page
+							Simple. Sleek. Modern. Get More Downloads By Wowing Customers With
+							A Captivating Landing Page
 						</a>
 					</p>
-				</div>
-			</section>
-			<section className="spotlight">
-				<div className="image">
-					<img src={pic3} alt="Gold+Sage Logo" />
-				</div>
-				<div className="content">
-					<h2>
-						<a href="">Gold + Sage Jewelry Website</a>
-					</h2>
-					<p>Gold + Sage eCommerce website coming soon!</p>
 				</div>
 			</section>
 		</section>
